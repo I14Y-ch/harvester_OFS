7fa4b1e6015e1d5d9d72ba3be03d9fd98aeeb40e
Lien Beroggi <163211986+lberoggi@users.noreply.github.com>
2025-04-04 10:59:05 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,13 @@
+import os
+
+# OGD canton Basel-Landschaft API
+API_OFS_URL = "https://dam-api.bfs.admin.ch/hub/api/ogd/harvest"
+
 # I14Y API configuration
 API_BASE_URL = "https://api-a.i14y.admin.ch/api/partner/v1"
-API_TOKEN = "Bearer eyJraWQiOiI5YT"
+API_TOKEN = f"{os.environ['ACCESS_TOKEN']}" 
+
+#IDS_I14Y = json.loads(os.environ['IDS_I14Y'])
 
 # Organization settings
 ORGANIZATION_ID = "CH1"
@@ -8,23 +15,11 @@ DEFAULT_PUBLISHER = {
     "identifier": ORGANIZATION_ID
 }
 
-# File paths
-TEMPLATE_PATH = "api_responses/response_1.xml"
-
 # File format (.xml and .rdf -> "xml", .ttl -> "ttl")
 FILE_FORMAT = "xml"
 
-
-# Configuration from config.py
-API_BASE_URL = API_BASE_URL
-API_TOKEN = API_TOKEN
-ORGANIZATION_ID = ORGANIZATION_ID
-DEFAULT_PUBLISHER = DEFAULT_PUBLISHER
-TEMPLATE_PATH = TEMPLATE_PATH
-FILE_FORMAT = FILE_FORMAT
-
-# Proxy configuration (if needed)
+# Proxies if necessary 
 PROXIES = {
-    "http": os.environ.get('HTTP_PROXY', ''),
-    "https": os.environ.get('HTTPS_PROXY', '')
+    "http": "http://proxy...",
+    "https": "http://proxy..."
 }
