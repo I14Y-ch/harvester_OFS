06614d38c564acf19c36993803d5b3b9aa20ea82
Sergiy Goloviatinski <sergiy.goloviatinski@bfs.admin.ch>
2025-12-10 10:02:54 +0100

    Changed GET_TOKEN_URL

--- src/config.py
+++ src/config.py
@@ -7,8 +7,8 @@ API_BASE_URL = "https://api.i14y.admin.ch/api/partner/v1"
 API_BASE_URL_ABN = "https://api-a.i14y.admin.ch/api/partner/v1"
 
 GET_TOKEN_URL_DEV = "https://identity-eiam-r.eiam.admin.ch/realms/edi_bfs-i14y"
-GET_TOKEN_URL_ABN = "https://identity-a.bit.admin.ch/realms/bfs-sis-a/protocol/openid-connect/token"
-GET_TOKEN_URL_PROD = "https://identity.bit.admin.ch/realms/bfs-sis-p/protocol/openid-connect/token"
+GET_TOKEN_URL_ABN = "https://identity.i14y.a.c.bfs.admin.ch/realms/bfs-sis-a/protocol/openid-connect/token"
+GET_TOKEN_URL_PROD = "https://identity.i14y.c.bfs.admin.ch/realms/bfs-sis-p/protocol/openid-connect/token"
 
 # Organization settings
 ORGANIZATION_ID =  "CH1"
